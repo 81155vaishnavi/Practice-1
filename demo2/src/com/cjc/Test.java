2926068b45c91e5310512db95c0f6164a093d39b
admin <admin@DESKTOP-IDFE2KR>
2024-09-20 15:51:57 +0530

    by ABC1

---
+++ demo2/src/com/cjc/Test.java
@@ -0,0 +1,21 @@
+package com.cjc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is demo2 project");
+
+	}
+	
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
+	
+	public void m2()
+	{
+		System.out.println("This is m2 method");
+	}
+
+}
